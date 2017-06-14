01602d3e7399ae441c03090cab2ef9ee6746576c
Chao Huang <hchao8@gmail.com>
2017-06-14 09:59:05 -0400

    more

--- todo/index.ios.js
+++
@@ -1,6 +0,0 @@
-import { AppRegistry } from 'react-native'
-import CrossTodo from './src/main'
-
-// AppRegistry.registerComponent('CopyFromExponent', () => ;
-
-AppRegistry.registerComponent('AwesomeProject', () => CrossTodo)
